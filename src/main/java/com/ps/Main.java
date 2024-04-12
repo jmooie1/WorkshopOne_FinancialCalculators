0b0cbde183350236328989c451e998c08f4e68aa
Jason <jmdestiny13@gmail.com>
2024-04-12 16:08:24 -0400

    Question 1 for Workshop 1

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,7 +1,31 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the principal amount: $ ");
+        double principal = scanner.nextDouble();
+
+        System.out.println("Please enter the interest rate %: ");
+        double annualInterestRate = scanner.nextDouble();
+
+        System.out.println("Please enter the loan length (in years): ");
+        int years = scanner.nextInt();
+
+        double monthlyPayment = calculateMonthlyPayment(principal, annualInterestRate, years);
+
+        double totalInterestPaid = monthlyPayment * years * 12 - principal;
+
+        System.out.println("A $" + principal + " loan at " + annualInterestRate + " interest for " + years + " years would have a $" + String.format("%.2f",monthlyPayment) + "/mo payment with a total interest of $" + String.format("%.2f", totalInterestPaid));
+    }
+
+    public static double calculateMonthlyPayment(double principal, double annualInterestRate, int years) {
+        double monthlyInterestRate = annualInterestRate / 12 / 100;
+        int months = years * 12;
+        double monthlyPayment = principal * monthlyInterestRate * Math.pow(1 + monthlyInterestRate, months) /(Math.pow(1 + monthlyInterestRate, months) - 1);
+        return monthlyPayment;
     }
 }
